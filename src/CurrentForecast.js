2d74145ccbd0bea0c8e6c3b5503f78403f772746
EllieSanghani <sangh@mac212589.local>
2020-11-07 15:14:30 +0000

    Celcius to Fahrenheit converter

--- src/CurrentForecast.js
+++ src/CurrentForecast.js
@@ -1,27 +1,78 @@
-import React from "react";
+import React, {useState} from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./CurrentForecastStyles.css";
 
 export default function CurrentForecast(props) {
-  return (
-    <div id="forcast">
+const [unit, setUnit] = useState("metrics"); 
+
+
+
+function convertToFahrenheit(event) {
+  event.preventDefault(); 
+  setUnit("imperial");
+}
+
+function convertToCelcius(event) {
+  event.preventDefault(); 
+  setUnit("metrics");
+}
+
+
+
+if (unit === "metrics") {
+let celcius = props.data.temp;
+return (
+  
+  <div id="forcast">
       <WeatherIcon code={props.data.icon} alt={props.data.description} />
      
-      <strong className="dayTemp">{props.data.temp}</strong>
+
+      <strong className="dayTemp">{celcius}</strong>
 ​
       <div id="converter">
-        <button className="cTemp">
-          <a className="active" id="celcius" href="#/">
+         
             °C
-          </a>
-        </button>
+        
         <small className="divider"> | </small>
         <button className="fTemp">
-          <a id="fahrenheit" href="#/">
+          <a id="fahrenheit" href="/" onClick={convertToFahrenheit}>
             °F
           </a>
         </button>
       </div>
     </div>
   );
+} else {
+
+  let celcius = props.data.temp;
+  let fahrenheit = (celcius * 9 / 5) + 32;
+  return (
+
+<div id="forcast">
+      <WeatherIcon code={props.data.icon} alt={props.data.description} />
+     
+
+      <strong className="dayTemp">{fahrenheit}</strong>
+​
+      <div id="converter">
+        <button className="cTemp">
+         
+          <a id="celcius" href="/" onClick={convertToCelcius}> 
+            °C
+          </a>
+        </button>
+        <small className="divider"> | </small>
+
+            °F
+      
+      </div>
+    </div>
+
+  )
 }
+
+
+}
+
+
+
